9a75bd3f15057983314d61410a402816bee13435
Yi Feng Yang <yifeng@yfyang.dev>
2020-08-01 15:07:04 -0400

    Refactor into modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #[macro_use]
 extern crate lazy_static;
 
+pub use feed::EcWeatherFeed;
+pub use feed::InitError;
+
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -9,52 +13,4 @@ mod tests {
     }
 }
 
-lazy_static! {
-    static ref ACCEPTED_LANGS: Vec<&'static str> = vec!["en", "fr"];
-}
-
-#[derive(Debug)]
-pub struct EcWeatherFeed {
-    pub city_code: String,
-    pub lang: String,
-}
-
-pub struct InitError {
-    pub message: String
-}
-
-impl EcWeatherFeed {
-    pub fn new(city_code: String, lang: String) -> Result<EcWeatherFeed, InitError> {
-        if !ACCEPTED_LANGS.contains(&lang.as_str()) {
-            return Err(
-                InitError {
-                    message: format!(
-                        "Invalid lang supplied: \"{}\". Accepted langs are {}.",
-                        lang,
-                        ACCEPTED_LANGS.join(", "))
-                }
-            );
-        }
-        Ok(EcWeatherFeed {
-            city_code,
-            lang,
-        })
-    }
-
-    pub async fn query(&self) -> Result<String, reqwest::Error> {
-        let lang_char = self.lang.chars().nth(0).unwrap();
-        let url = format!("https://weather.gc.ca/rss/city/{city}_{lang}.xml", city = self.city_code, lang = lang_char);
-        let response = reqwest::get(url.as_str())
-            .await?;
-        // if not a 200 code, will Err
-        match response.error_for_status() {
-            Ok(res) => {
-                let text = res.text().await?;
-                Ok(text)
-            }
-            Err(e) => {
-                Err(e)
-            }
-        }
-    }
-}
+pub mod feed;
